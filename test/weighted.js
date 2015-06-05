88397c13a32df6ac0c403e0c31ac5b42f76ee7ae
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-06-05 10:09:29 -0700

    Add test suite for basic calling conventions.

--- test/weighted.js
+++ test/weighted.js
@@ -1,10 +1,60 @@
 var weighted = require('../')
-  , optionsArr = ['Wake Up', 'Snooze']
-  , weightsArr = [0.25, 0.75]
-  , optionsObj = {
-      'Wake Up': 0.25
-    , 'Snooze': 0.75
-    }
+var expect = require('chai').expect
+var OPTIONS_ARR_5 = [1, 2, 3, 4, 5]
+var WEIGHTS_ARR_5 = [0.2, 0.4, 0.1, 0.2, 0.1]
+var WEIGHTS_ARR_4 = [0.2, 0.4, 0.2, 0.2]
+var OPTIONS_OBJ = OPTIONS_ARR_5.reduce(function (obj, key, index) {
+  obj[key] = WEIGHTS_ARR_5[index]
+  return obj
+}, {})
 
-console.log(weighted.select(optionsArr, weightsArr))
-console.log(weighted.select(optionsObj))
+describe('Weighted', function () {
+  it('should export a Function', function () {
+    expect(weighted).to.be.a('function')
+  })
+
+  it('should export as .select()', function () {
+    expect(weighted.select).to.be.a('function')
+    expect(weighted.select).to.equal(weighted)
+  })
+
+  describe('Array, Array => Value', function () {
+    it('should return a value', function () {
+      expect(weighted.select(OPTIONS_ARR_5, WEIGHTS_ARR_5)).to.be.a('number')
+    })
+
+    it('should accept Array, Null', function () {
+      expect(weighted.select(OPTIONS_ARR_5)).to.be.a('number')
+    })
+
+    it('should reject Array[m], Array[n] where m != n', function () {
+      expect(function () {
+        weighted.select(OPTIONS_ARR_5, WEIGHTS_ARR_4)
+      }).to.throw(TypeError)
+    })
+
+    it('should reject Array, Object', function () {
+      expect(function () {
+        weighted.select(OPTIONS_ARR_5, OPTIONS_OBJ)
+      }).to.throw(TypeError)
+    })
+
+    it('should reject Array, Primitive', function () {
+      expect(function () {
+        weighted.select(OPTIONS_ARR_5, 42)
+      }).to.throw(TypeError)
+    })
+  })
+
+  describe('Object => Value', function () {
+    it('should return a value', function () {
+      expect(weighted.select(OPTIONS_OBJ)).to.be.a('string')
+    })
+  })
+
+  it('should reject other calling forms', function () {
+    expect(function () {
+      weighted.select(1, 2, 3, 4, 5)
+    }).to.throw(TypeError)
+  })
+})
